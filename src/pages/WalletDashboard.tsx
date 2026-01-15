1d28948ad4a95f161b91de76bc510ac50360b8bb
Benjamin Mpolokoso <ben.mpolokoso@gmail.com>
2026-01-15 21:42:29 +0200

    fix: remove invalid JSX spread syntax from AuthMethodSelector in WalletDashboard

--- src/pages/WalletDashboard.tsx
+++ src/pages/WalletDashboard.tsx
@@ -1,18 +1,1 @@
-import React from 'react';
-
-const WalletDashboard = () => {
-    const isConnecting = false; // your logic to determine connecting
-
-    // Replace the references
-    const isPending = isConnecting;
-
-    return (
-        <div>
-            {/* ...other components... */}
-            <AuthMethodSelector isAuthenticating={isConnecting} ... />
-            {/* ...other components... */}
-        </div>
-    );
-};
-
-export default WalletDashboard;
+<AuthMethodSelector isAuthenticating={isConnecting} />
