798237f2e5e43e5715df9b9513a60974539f15a3
Benjamin Mpolokoso <ben.mpolokoso@gmail.com>
2026-01-23 05:36:22 +0000

    Fix Airdrop.tsx to use pre-instantiated airdropContract
    Co-authored-by: Ona <no-reply@ona.com>

--- src/pages/Airdrop.tsx
+++ src/pages/Airdrop.tsx
@@ -41,7 +41,7 @@ import { AfricaRailwaysQuiz } from "@/components/AfricaRailwaysQuiz";
 import { ReferralLeaderboard } from "@/components/ReferralLeaderboard";
 import ClaimButton from "@/components/ClaimButton";
 import { useActiveAccount, ConnectButton } from "thirdweb/react";
-import { client, polygon, AIRDROP_CONTRACT_ADDRESS } from "@/lib/thirdwebClient";
+import { client, polygon, AIRDROP_CONTRACT_ADDRESS, airdropContract } from "@/lib/thirdwebClient";
 import { getAirdropStatus, verifyTwitter, verifyTelegram } from "@/services/airdropService";
 import { setActiveReferrer } from "@/services/referralService";
 import { useToast } from "@/hooks/use-toast";
@@ -279,12 +279,7 @@ export default function Airdrop() {
     progress.quizPassed
   ].filter(Boolean).length * 25;
 
-  // Get airdrop contract
-  const airdropContract = AIRDROP_CONTRACT_ADDRESS ? getContract({
-    client,
-    chain: polygon,
-    address: AIRDROP_CONTRACT_ADDRESS
-  }) : null;
+  // airdropContract is imported from thirdwebClient
 
   return (
     <AppLayout>
